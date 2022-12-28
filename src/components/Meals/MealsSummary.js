cd5058a4b30ad252b556bc6be34e65fab90c079c
Aydoan Balakchiev <balakchiev02@gmail.com>
2022-12-28 19:51:14 +0200

    Add meals lifecycle

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,16 @@
+import styles from './MealsSummary.module.css'
+
+function MealsSummary() {
+    return <section className={styles.summary}>
+        <h2>Delicious Food, delivered to you!</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto delectus dignissimos eveniet ex illo in
+            laudantium non possimus quis sed.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium blanditiis consectetur delectus,
+            exercitationem laudantium nam natus non qui sunt voluptatum.
+        </p>
+    </section>
+}
+
+export default MealsSummary;
