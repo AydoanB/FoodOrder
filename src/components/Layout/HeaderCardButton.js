1b6604a2bdee7509ff0a7130f8fc5268b018a239
Aydoan Balakchiev <balakchiev02@gmail.com>
2022-12-30 13:22:27 +0200

    UseEffect for rerendering cart component on add

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -1,12 +1,34 @@
 import CartIcon from "../Card/CartIcon";
 import styles from './HeaderCardButton.module.css'
-import {useContext} from "react";
+import {useContext, useEffect, useState} from "react";
 import CartContext from "../../store/cart-context";
 
 function HeaderCartButton(props) {
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
     const ctx = useContext(CartContext)
+    const numberOfItemsInCart = ctx.items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    }, 0);
 
-    return <button onClick={props.onClick} className={styles.button}>
+    const btnClasses = `${styles.button} ${btnIsHighlighted ? styles.bump : ''}`;
+
+    useEffect(() => {
+        if (ctx.items.length === 0) {
+            return;
+        }
+
+        setBtnIsHighlighted(true);
+
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false);
+        }, 300)
+
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [ctx.items]);
+
+    return <button onClick={props.onClick} className={btnClasses}>
         <span className={styles.icon}>
             <CartIcon/>
         </span>
@@ -14,7 +36,7 @@ function HeaderCartButton(props) {
             Your cart
         </span>
         <span className={styles.badge}>
-            {ctx.items.length}
+            {numberOfItemsInCart}
         </span>
     </button>
 }
