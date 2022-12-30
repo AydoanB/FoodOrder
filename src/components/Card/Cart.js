1da6da2db0e391a93b568f7ebd6b6f51e881617c
Aydoan Balakchiev <balakchiev02@gmail.com>
2022-12-30 13:21:35 +0200

    Replace hardcoded items in cart with dynamically added ones

--- src/components/Card/Cart.js
+++ src/components/Card/Cart.js
@@ -1,22 +1,41 @@
 import styles from './Cart.module.css'
 import Modal from "../UI/Modal";
+import {useContext} from "react";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 
 function Cart(props) {
+    const ctx = useContext(CartContext);
+
+    const cartItemAddHandler = (item) => {
+        ctx.addItem({...item, amount: 1});
+    }
+    const cartItemRemovedHandler = (id) => {
+        ctx.removeItem(id);
+    }
+
+    const totalAmount = `$${ctx.totalAmount.toFixed(2)}`;
+    const hasItems = ctx.items.length > 0;
 
     const cartItems = <ul className={styles['cart-items']}>
-        {[{id: 'c1', name: 'Sushi', amount: 2, price: 12}]
-            .map((item) => <li>{item.name}</li>)}
+        {ctx.items.map((item) => <CartItem
+            key={item.id}
+            name={item.name}
+            amount={item.amount}
+            price={item.price}
+            onAdd={cartItemAddHandler.bind(null, item)}
+            onRemove={cartItemRemovedHandler.bind(null, item.id)}/>)}
     </ul>;
 
     return (<Modal onClose={props.onClose}>
         {cartItems}
         <div className={styles.total}>
             <span>Total Amount</span>
-            <span>35.26</span>
+            <span>{totalAmount}</span>
         </div>
         <div className={styles.actions}>
             <button onClick={props.onClose} className={styles['button--alt']}>Close</button>
-            <button className={styles.button}>Order</button>
+            {hasItems && <button className={styles.button}>Order</button>}
         </div>
     </Modal>)
 }
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -30,10 +30,12 @@ const DUMMY_MEALS = [
 ];
 
 function AvailableMeals() {
-    const mealsList = DUMMY_MEALS.map(meal => <MealItem key={meal.id}
-                                                        name={meal.name}
-                                                        description={meal.description}
-                                                        price={meal.price}></MealItem>);
+    const mealsList = DUMMY_MEALS.map(meal => <MealItem
+        id={meal.id}
+        key={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}></MealItem>);
 
     return <section className={styles.meals}>
         <ul>
